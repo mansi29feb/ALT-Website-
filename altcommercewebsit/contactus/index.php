b30dca1bbc3ecc73549d85f4b03bba6b68e97011
mukunda <mukunda@altcommerce.com>
2020-04-23 11:59:01 +0530

    modified by kavya

--- altcommercewebsit/contactus/index.php
+++ altcommercewebsit/contactus/index.php
@@ -3,105 +3,66 @@ include_once "../header.php";
 ?>
 
 <!--contact us start-->
-<div class="uk-container uk-margin-medium-bottom uk-margin-medium-top">
-    
-    <div class="uk-child-width-1-2@m uk-grid-small uk-child-width-1-1@s uk-padding-small" uk-grid>
+<div class="uk-container uk-margin uk-margin-large-bottom ">
+    <div class="uk-child-width-1-1@s uk-grid-small" uk-grid>    
+        <div class="uk-h2 uk-text-center uk-text-bold services_mainheading">Get in Touch</div>
+
+        <div class="uk-width-1-2@s uk-align-center">
+                <p class="services_mainpara uk-card uk-card-default uk-card-body uk-box-shadow-medium uk-padding-small uk-text-center uk-card-hover ">“ Don't ignore the effort of a person <br> who tries to keep in touch.
+                                It is not all the <br>time someone cares  ~Anonymous ”
+                </p>
+        </div>
+        
+        <div class="uk-child-width-1-2@m uk-grid-small uk-child-width-1-1@s uk-padding-small" uk-grid>
             
-        <div class="uk-width-1-2@m">
-            <div class="uk-h3 uk-text-left services_article_heading uk-margin-small-top uk-text-bold">Send us a Message</div>
-            <div class="career_para uk-text-left">At ALT Commerce, our goal is to deliver solutions that 
-                exactly meet the business objectives of our clients. We look forward to learning how we can help you.</div><br>
+                <div class="uk-width-1-2@m">
+                    <div class="uk-text-center">
+                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdNl0WC5Kx-jqt27eq7ppyLZsLmVDvGlIMvObK_Mmi18Shtyg/viewform?embedded=true" width="600" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                    </div> 
+                </div>
+                <div class="uk-width-1-2@m">
+                    <div class="uk-text-center">
+                        <div class="uk-h1  uk-margin-small-top uk-text-bold"><span style="color:#ff7600;"> Our</span>&nbsp;Office</div>
+                        <div class="uk-inline">
+                            <div class="case_card uk-card uk-card-default uk-margin uk-card-hover uk-transition-toggle" tabindex="0" >
+                            
+                                <img class="uk-transition-scale-up" src="<?php echo $filePath;?>images/noida.jpg" alt="" style="opacity: 0.9; width: 300px; height: 222px;">
+                                <div class="uk-overlay uk-position-center"><button type="button" class="uk-button uk-button-text" ><a  class="cases_btnlink" href="https://goo.gl/maps/yn6EXoLCaQzXEMEY9" style="color:white;" target="_blank">CORPORATE OFFICE </a></button>
+                                </div>
+            
+                        </div>
+                    </div>
+                    <div class="uk-inline">
 
-            <form class="uk-form-stacked" action="<?php echo $filePath;?>sendmail/index.php" method="post">
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-text">NAME*</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="text" placeholder="" name="name">
+                        <div class="case_card uk-card uk-card-default uk-margin uk-card-hover uk-transition-toggle" tabindex="0" >
+                        
+                            <img class="uk-transition-scale-up" src="<?php echo $filePath;?>images/Bangalore.jpg" alt="" style="opacity: 0.9; width: 300px; height: 222px;">
+                            <div class="uk-overlay uk-position-center"><button type="button" class="uk-button uk-button-text" ><a  class="cases_btnlink" href="https://goo.gl/maps/ycdkDSCKBfi7pqc76" style="color:white;" target="_blank">BANGALORE OFFICE </a></button>
+                            </div>
+            
+                        </div>
                     </div>
-                </div>
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-s-email">EMAIL*</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" id="form-s-email" type="email" placeholder="" name="email">
-                    </div>
-                </div>
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-text">SUBJECT*</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="text" placeholder="" name="subject">
-                    </div>
-                </div>
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-text">MESSAGE*</label>
-                    <textarea class="uk-textarea" rows="5" placeholder="" name="msg"></textarea>
-                </div>
-                
-                <div class="uk-margin">
-                    <div class="g-recaptcha" data-sitekey="6Lcw6HcUAAAAALgTE9ImQ-5lSlG0-uRuIvsLxvTX" data-callback="verifyRecaptchaCallback" data-expired-callback="expiredRecaptchaCallback"></div>
-                    <input style="display:none" class="form-control d-none" data-recaptcha="true"  data-error="Please complete the Captcha">
-                    <div class="help-block with-errors"></div>
-                </div><br>
-                <button class="uk-button uk-button-default sub-button">SEND MESSAGE</button>
-            </form>  
-        </div> 
-        <div class="uk-width-1-2@m">
-
-            <div class="uk-text-center">
-                <div class="uk-h3  services_article_heading uk-margin-small-top uk-text-bold">Get in Touch</div> 
-                <div class="career_para">Don't ignore the effort of a person <br> who tries to keep in touch.
-                        It is not all the <br>time someone cares  "~Anonymous" </div><br>
-                    <span class="uk-margin-small-right" uk-icon="location"></span>&nbsp;CORPORATE OFFICE
-                    <p>ALT COMMERCE PRIVATE LIMITED.
-                    <br> Address :#1211, Tower B, 12 Floor, i-thum, A-40 ,sector 62,<br> Noida – 201309</p>
-                    <span class="uk-margin-small-right" uk-icon="location"></span>&nbsp;BANGALORE OFFICE 
-                    <p>WORK DISTRICT <br> Address :#46, 7th B Main Rd, 4th Block, Jayanagar,<br> Bengaluru – 560041</p>
-                        <!----> 
-                        <div class="">
+                    
+                    <div class="uk-text-center uk-margin-top">
                             <span uk-icon="receiver" class="uk-icon">
                             <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="receiver"><path fill="none" stroke="#000" stroke-width="1.01" d="M6.189,13.611C8.134,15.525 11.097,18.239 13.867,18.257C16.47,18.275 18.2,16.241 18.2,16.241L14.509,12.551L11.539,13.639L6.189,8.29L7.313,5.355L3.76,1.8C3.76,1.8 1.732,3.537 1.7,6.092C1.667,8.809 4.347,11.738 6.189,13.611"></path></svg></span>&nbsp;Call Us:<br>
-                            <div class="uk-text-bold uk-text-secondary contact_text">
-                                +911204226757<br><br>
+                            <div class="uk-text-bold uk-text-secondary contact_text" style="color:#ff7600;">
+                                0120-4226757<br><br>
                              </div>
                         
                              <span uk-icon="mail" class="uk-icon"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="mail"><polyline fill="none" stroke="#000" points="1.4,6.5 10,11 18.6,6.5"></polyline><path d="M 1,4 1,16 19,16 19,4 1,4 Z M 18,15 2,15 2,5 18,5 18,15 Z"></path></svg></span>&nbsp;Email Us:<br> 
                            <div class="uk-text-bold uk-text-secondary contact_text">
                             info@altcommerce.com</div>
-                        </div> 
-
-
-             </div>
-            
+                    </div> 
+                </div>
+            </div>
         </div>
         
     </div>
 </div>
-<div class="uk-container-fluid">
-<div class="uk-child-width-expand@s ">
-        <!--
-                <iframe  frameborder="0" 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.6470254577785!2d77.580223149983!3d12.930393290838163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1551a8fe2871%3A0x55a7248ef808e68b!2sALT%20Commerce!5e0!3m2!1sen!2sin!4v1584605675234!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-                </iframe>
-    -->
-    
-    <!-- map container start -->
-    <div class="map-container">
-        <div id="map-canvas"></div>
-    </div>
-    
-    <!-- map container end -->
-    <style>
-        #map-canvas{height:100%;}
-        .map-container {
-    height: 450px;
-    position: relative;
-    margin: 0;
-    padding: 0;
-}
-    </style>
-              
-</div>
-</div>
-<script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyDjQ1OrapthKCWtfDX9tlbkr84xRW8RMSk"></script>
+
+
+
 
 <!--contact us end -->
 <?php
