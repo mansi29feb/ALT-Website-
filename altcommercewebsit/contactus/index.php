61e0aa36cde0eef12325a6a14f39f5df924a9c74
mukunda <mukunda@altcommerce.com>
2020-04-23 17:52:08 +0530

    modified by kavya

--- altcommercewebsit/contactus/index.php
+++ altcommercewebsit/contactus/index.php
@@ -15,11 +15,11 @@ include_once "../header.php";
         
         <div class="uk-child-width-1-2@m uk-grid-small uk-child-width-1-1@s uk-padding-small" uk-grid>
             
-                <div class="uk-width-1-2@m">
+                <div class="uk-width-1-2@m" grid>
                     <div class="uk-text-center">
                         
                             
-                                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdNl0WC5Kx-jqt27eq7ppyLZsLmVDvGlIMvObK_Mmi18Shtyg/viewform?embedded=true" width="600" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdNl0WC5Kx-jqt27eq7ppyLZsLmVDvGlIMvObK_Mmi18Shtyg/viewform?embedded=true" width="550" height="1000">Loading…</iframe>
                     </div> 
                 </div>
                 <div class="uk-width-1-2@m">
@@ -49,7 +49,7 @@ include_once "../header.php";
                             <span uk-icon="receiver" class="uk-icon">
                             <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="receiver"><path fill="none" stroke="#000" stroke-width="1.01" d="M6.189,13.611C8.134,15.525 11.097,18.239 13.867,18.257C16.47,18.275 18.2,16.241 18.2,16.241L14.509,12.551L11.539,13.639L6.189,8.29L7.313,5.355L3.76,1.8C3.76,1.8 1.732,3.537 1.7,6.092C1.667,8.809 4.347,11.738 6.189,13.611"></path></svg></span>&nbsp;Call Us:<br>
                             <div class="uk-text-bold uk-text-secondary contact_text" style="color:#ff7600;">
-                                +911204226757<br><br>
+                            +911204226757<br><br>
                              </div>
                         
                              <span uk-icon="mail" class="uk-icon"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="mail"><polyline fill="none" stroke="#000" points="1.4,6.5 10,11 18.6,6.5"></polyline><path d="M 1,4 1,16 19,16 19,4 1,4 Z M 18,15 2,15 2,5 18,5 18,15 Z"></path></svg></span>&nbsp;Email Us:<br> 
