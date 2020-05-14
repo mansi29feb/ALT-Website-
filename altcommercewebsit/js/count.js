e36617a3b1e26dee69977e8f69ec47c8d67379fc
mukunda <mukunda@altcommerce.com>
2020-05-14 12:56:32 +0530

    preloader done by  kavya

--- altcommercewebsit/js/count.js
+++ altcommercewebsit/js/count.js
@@ -10,6 +10,21 @@ $('.Count').each(function () {
     });
 });
 
+//preloader
+$(document).ready(function($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+    
+    });
+function preloadeSite(){
+
+    $('.preloader-wrapper').fadeOut();
+    $('body').removeClass('preloader-site');
+
+}
+
+window.addEventListener('load', preloadeSite);
+
 
 
     
