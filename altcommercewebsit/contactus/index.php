bb5ec199d479fdbae7166efa2fc19b2a184da897
mukunda <mukunda@altcommerce.com>
2020-05-29 17:05:16 +0530

    modified conctact form

--- altcommercewebsit/contactus/index.php
+++ altcommercewebsit/contactus/index.php
@@ -14,13 +14,11 @@
             <div class="services_mainpara uk-card uk-card-default uk-padding-small uk-text-center  ">“ We do look forward to keeping in touch with you guys, because we'll always be connected in people's mind ”
             <p class="uk-text-meta uk-text-right uk-margin-remove-top uk-margin-remove-bottom" style="color:black;">- Barry Zito</p>
             </div>
-
         </div>
-        
         <div class="uk-child-width-1-2@m uk-grid-small uk-child-width-1-1@s uk-padding-small" uk-grid>
             <div class="uk-width-1-2@m">
                 <div class="uk-text-center">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdNl0WC5Kx-jqt27eq7ppyLZsLmVDvGlIMvObK_Mmi18Shtyg/viewform?embedded=true" style="width:100%; height:1150px;" ></iframe>
+                  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdNl0WC5Kx-jqt27eq7ppyLZsLmVDvGlIMvObK_Mmi18Shtyg/viewform?embedded=true" style="width:100%; height:1150px;" ></iframe>
                 </div> 
             </div>
                 <div class="uk-width-1-2@m">
@@ -80,7 +78,5 @@
 <?php
 include_once "../footer.php"
 ?>
-
 </body>
-
 </html>
